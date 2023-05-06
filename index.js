56d500f2e6108431b5bebc6fdb1c23f9f897e0a4
Everton <everton.rodrigues1104@gmail.com>
2023-05-06 20:53:00 -0300

    msg ok

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
-require("chromedriver");
+require("selenium-webdriver/chrome");
 
 (async () => {
     let pivo = 'wp-selenium'
@@ -12,25 +12,29 @@ require("chromedriver");
                 await driver.findElement(By.xpath('//span[@title="' + pivo + '"]')).click()
             }
             try {
-                await driver.wait(until.elementLocated(By.xpath('//span[@data-testid="icon-unread-count"]')), 5000);
-                if (await driver.findElement(By.xpath('//span[@data-testid="icon-unread-count"]')).isDisplayed()) {
-                    await driver.findElement(By.xpath('//span[@data-testid="icon-unread-count"]')).click()
+                await driver.wait(until.elementLocated(By.xpath('//span[contains(@aria-label,"não lida")]')), 5000);
+                if (await driver.findElement(By.xpath('//span[contains(@aria-label,"não lida")]')).isDisplayed()) {
+                    await driver.findElement(By.xpath('//span[contains(@aria-label,"não lida")]')).click()
                 }
                 try {
-                    await driver.wait(until.elementsLocated(By.xpath('//div[@data-testid="msg-container"]')), 5000);
-                    let container = await driver.findElements(By.xpath('//div[@data-testid="msg-container"]'))
-                    let msgs = await container[0].findElements(By.xpath('//div[@data-testid="msg-container"]/div[1]/div[1]/div[1]/div/span[1]/span'))// não funciona em grupos
-                    console.log('penultima msg->' + await msgs[msgs.length - 2].getText());
-                    console.log('ultima msg->' + await msgs[msgs.length - 1].getText());
+                    sendMsg(driver, 'ola, eu sou o chatbot do whatsapp')
+                    // await driver.wait(until.elementsLocated(By.xpath('//div[@data-testid="msg-container"]')), 5000);
+                    // let container = await driver.findElements(By.xpath('//div[@data-testid="msg-container"]'))
+                    // let msgs = await container[0].findElements(By.xpath('//div[@data-testid="msg-container"]/div[1]/div[1]/div[1]/div/span[1]/span'))// não funciona em grupos
+                    // console.log('penultima msg->' + await msgs[msgs.length - 2].getText());
+                    // console.log('ultima msg->' + await msgs[msgs.length - 1].getText());
+                    // sendMsg(driver, 'ola, eu sou o chatbot do whatsapp');
                 } catch (error) { }
             } catch (error) { }
         } catch (error) { }
     }, 5000)
 })()
 
+
+// sendMsg(driver, 'ola, eu sou o chatbot do whatsapp');
 async function sendMsg(driver, txt) {
     setTimeout(() => { }, 500)
-    await driver.wait(until.elementLocated(By.xpath('//div[@data-lexical-editor="true"]')), 5000);
-    let msg = driver.findElement(By.xpath('//div[@data-lexical-editor="true"]'))
+    await driver.wait(until.elementLocated(By.xpath('//div[@title="Mensagem"]')), 5000);
+    let msg = driver.findElement(By.xpath('//div[@title="Mensagem"]'))
     msg.sendKeys(txt, Key.ENTER)
 }
